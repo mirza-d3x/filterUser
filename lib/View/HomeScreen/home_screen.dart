5b8e78022823ad99ba9c9d15e96d848105f9e152
mirza-d3x <mohammedmirsa010@gmail.com>
2022-12-13 13:31:07 +0530

    home screen design completed

--- lib/View/HomeScreen/home_screen.dart
+++ lib/View/HomeScreen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:creativelabs/Model/get_user_details.dart';
+import 'package:creativelabs/Repository/Bloc/User/user_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ScreenHome extends StatefulWidget {
   const ScreenHome({Key? key}) : super(key: key);
@@ -9,70 +12,103 @@ class ScreenHome extends StatefulWidget {
 
 class _ScreenHomeState extends State<ScreenHome> {
   @override
+  void initState() {
+    BlocProvider.of<UserBloc>(context).add(GetAllUserDetailsEvent());
+    super.initState();
+  }
+
+  late GetUserDetailsModel getUserDetailsModel;
+
+  @override
   Widget build(BuildContext context) {
     final mHeight = MediaQuery.of(context).size.height;
     final mWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          height: mHeight,
-          width: mWidth,
-          child: Column(
-            children: [
-              Container(
-                height: mHeight * .1,
-                padding: EdgeInsets.symmetric(
-                  vertical: mHeight * .01,
-                  horizontal: mWidth * .03,
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.grey.withOpacity(.5),
-                    filled: true,
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.grey.withOpacity(.5),
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(
-                        color: Colors.grey.withOpacity(.5),
-                      ),
-                    ),
-                    hintText: "Search...",
-                    hintStyle: const TextStyle(color: Colors.grey),
-                    prefixIcon: const Icon(Icons.search),
-                    contentPadding: EdgeInsets.only(top: mHeight * .025),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.amber,
-                  child: ListView.builder(
-                    itemCount: 5,
-                    itemBuilder: (BuildContext context, int index) => Container(
+        body: BlocBuilder<UserBloc, UserState>(
+          builder: (context, state) {
+            if (state is UserLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is UserError) {
+              return Center(
+                child: Text('Error'),
+              );
+            }
+            if (state is UserLoaded) {
+              getUserDetailsModel =
+                  BlocProvider.of<UserBloc>(context).getUserDetailsModel;
+              return Container(
+                height: mHeight,
+                width: mWidth,
+                child: Column(
+                  children: [
+                    Container(
                       height: mHeight * .1,
-                      color: Colors.green,
-                      margin: EdgeInsets.only(
-                        top: index == 0 ? 0 : mHeight * .01,
+                      padding: EdgeInsets.symmetric(
+                        vertical: mHeight * .01,
+                        horizontal: mWidth * .03,
                       ),
-                      child: const ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.blue,
-                          radius: 25,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey.withOpacity(.5),
+                          filled: true,
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.grey.withOpacity(.5),
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              color: Colors.grey.withOpacity(.5),
+                            ),
+                          ),
+                          hintText: "Search...",
+                          hintStyle: const TextStyle(color: Colors.grey),
+                          prefixIcon: const Icon(Icons.search),
+                          contentPadding: EdgeInsets.only(top: mHeight * .025),
                         ),
-                        title: Text('Name'),
-                        subtitle: Text('message'),
                       ),
                     ),
-                  ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.amber,
+                        child: ListView.builder(
+                          itemCount: getUserDetailsModel.data!.length,
+                          itemBuilder: (BuildContext context, int index) =>
+                              Container(
+                            height: mHeight * .1,
+                            color: Colors.green,
+                            margin: EdgeInsets.only(
+                              top: index == 0 ? 0 : mHeight * .01,
+                            ),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                radius: 25,
+                                backgroundImage: NetworkImage(
+                                    getUserDetailsModel
+                                        .data![index].profilePic!),
+                              ),
+                              title:
+                                  Text(getUserDetailsModel.data![index].name!),
+                              subtitle:
+                                  Text(getUserDetailsModel.data![index].about!),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
+              );
+            }
+            return Container();
+          },
         ),
       ),
     );
